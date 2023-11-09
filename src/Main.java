87b84e98730d4148205601c46dac94375be5eb2f
Italo Monteiro <mont.italo5@gmail.com>
2023-11-09 07:53:14 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {System.out.println("Hello world!");
+
+        Pessoa qualquer = new Pessoa();
+        qualquer.setNome("rebeca andrade");
+        System.out.println(qualquer.getNome());
+
+    }
+}
